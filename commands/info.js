7af84303da134c049216c7c9ab2b43e199113c72
ichbinheimdall <ichbinheimdall@users.noreply.github.com>
2022-06-26 01:04:55 +0300

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -1,7 +1,4 @@
 const { MessageEmbed } = require('discord.js');
-const moment = require("moment")
-require("moment-duration-format")
-moment.locale("tr")
 
 module.exports.config = { 
     name: 'info',
@@ -10,13 +7,6 @@ module.exports.config = {
 
 module.exports.sex = async(client, message, args, config) => {
 
-message.channel.send(new MessageEmbed().setColor("#EB459E").setAuthor(message.author.tag, message.author.avatarURL({ dynamic: true }))
-                     .setDescription(`
-<a:hologram:907617268173783060> **${client.guilds.cache.size.toLocaleString()}** sunucuya hizmet veriyorum.
-<a:hologram:907617268173783060> **${client.guilds.cache.reduce((acc, currentValue) => acc + currentValue.memberCount, 0)}** kullanıcıya hizmet veriyorum.
-<a:hologram:907617268173783060> Pingim: **${client.ws.ping}**
-<a:hologram:907617268173783060> **${moment.duration(client.uptime).format(" D [gün], H [saat], m [dakika], s [saniye]")}** dir aktifim.
-        `)
-                     .setTimestamp().setFooter(`${config.EmbedFooter}`));
+message.channel.send(new MessageEmbed().setColor("#EB459E").setAuthor(message.author.tag, message.author.avatarURL({ dynamic: true })).setDescription(`<a:hologram:907617268173783060> **${client.guilds.cache.size.toLocaleString()}** sunucuya hizmet veriyorum. <a:hologram:907617268173783060> **${client.guilds.cache.reduce((acc, currentValue) => acc + currentValue.memberCount, 0)}** kullanıcıya hizmet veriyorum. <a:hologram:907617268173783060> Pingim: **${client.ws.ping}**`).setTimestamp().setFooter(`${config.EmbedFooter}`));
 
 };
